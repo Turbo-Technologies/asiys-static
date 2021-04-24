f24bd98dcfcbe036afc5bb47917fd91a22f306b2
Ishan Tiwari <poet.infamous@gmail.com>
2021-04-24 11:19:46 +0530

    ADDED menu

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './css/App.css'
+import Menu from "./Menu.jsx";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {menuopen:false, menu:'', displaymain:'block'}
+    this.openmenu = this.openmenu.bind(this);
+  }
+
+  openmenu(){
+    if(this.state.menuopen==false) {
+      this.setState({displaymain:'none'})
+      this.setState({menu:<Menu></Menu>})
+      this.setState({menuopen:true})
+    }
+    else {
+      this.setState({displaymain:'block'})
+      this.setState({menu:''})
+      this.setState({menuopen:false})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <h2 style={{flexGrow:1}}>Anand Ashram</h2>
+          <button className='btn btn-primary' onClick={this.openmenu}>Menu</button>
+        </nav>
+        {this.state.menu}
+        <main style={{display:this.state.displaymain}}>
+        <div id='heroimage'>
+          <h3>Learn Yoga the best way.</h3>
+        </div>
+        <div id='section'>
+          <div>Hello</div>
+          <div>M</div>
+        </div>
+        </main>
+      </div>
+    )
+  }
+}
---
+++ src/Menu.jsx
@@ -0,0 +1,12 @@
+import './css/menu.css'
+
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <section>
+            <a href="">About us</a>
+            <a href="">Book Class</a>
+        </section>
+    )
+}
