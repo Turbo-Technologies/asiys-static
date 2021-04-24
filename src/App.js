c9c4fa7ed595b4ac0a169afadca5133ab184f88d
Ishan Tiwari <poet.infamous@gmail.com>
2021-04-24 09:43:17 +0530

    navbar almost complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <nav>
+          <h1 style={{flexGrow:1}}>Anand Ashram</h1>
+          <button>Menu</button>
+        </nav>
+        <div id='heroimage'>
+          
+        </div>
       </div>
     )
   }
 }
-
-
-export default App;
