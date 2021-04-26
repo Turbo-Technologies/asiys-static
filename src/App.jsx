999a5f38d0bf4da54c5786cfcfc35db8764c11cb
Ishan Tiwari <poet.infamous@gmail.com>
2021-04-26 11:21:21 +0530

    redesigned the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import './css/App.css'
-import Menu from "./Menu.jsx";
-import menuicon from './menu.png'
+import './css/App.css';
+import ashramboys from './anandashram.jpg';
+import heroimage from './anandimage.jpg';
 
 export default class App extends Component {
   
 
   render() {
+    return(
+
+    <div id='container'>
+      <nav><h1>Anand Ashram</h1> freemium</nav>
+      <div id="main">
+        <img src={heroimage} alt=""/>
+        <h1>Anand Ashram</h1>
+        <p id='first'>Learn Yoga anytime anywhere</p>
+        <p id='second'>Thousands of tutorials</p>
+        <p id='third'>Absolutely free</p>
+        <button className='joinus'>Join now</button>
+      </div>
+      <div className="images">
+        <h1>Come and experience the bliss of yoga</h1>
+        <img src={ashramboys} alt=""/>
+      </div>
+
+      <p>  Turbo Technologies</p>
+    </div>
+    )
   }
 }
