2bc0b1b9d84be31ac208dff87c861d71fa55e27c
Ishan Tiwari <poet.infamous@gmail.com>
2021-04-26 09:15:21 +0530

    deleted everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
 import './css/App.css'
 import Menu from "./Menu.jsx";
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {menuopen:false, menu:'', displaymain:'block'}
-    this.openmenu = this.openmenu.bind(this);
-  }
+import menuicon from './menu.png'
 
-  openmenu(){
-    if(this.state.menuopen===false) {
-      this.setState({displaymain:'none'})
-      this.setState({menu:<Menu></Menu>})
-      this.setState({menuopen:true})
-    }
-    else {
-      this.setState({displaymain:'block'})
-      this.setState({menu:''})
-      this.setState({menuopen:false})
-    }
-  }
+export default class App extends Component {
+  
 
   render() {
-    return (
-      <div>
-        <nav>
-          <h2 style={{flexGrow:1}}>Anand Ashram</h2>
-          <button className='btn btn-primary' onClick={this.openmenu}>Menu</button>
-        </nav>
-        {this.state.menu}
-        <main style={{display:this.state.displaymain}}>
-        <div id='heroimage'>
-          <h3>Learn Yoga the best way.</h3>
-        </div>
-        <div id='section'>
-          <div>Hello</div>
-          <div>M</div>
-        </div>
-        </main>
-      </div>
-    )
   }
 }
